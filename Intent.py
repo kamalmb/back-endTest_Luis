c2cf6877d0bf41703a58d527a83e2076a546303e
kamalmb <73289762+kamalmb@users.noreply.github.com>
2023-03-23 02:34:32 +0100

    Update Intent.py

--- Intent.py
+++ Intent.py
@@ -20,9 +20,9 @@ class LuisIntent:
         url=self.endpoint+'/luis/prediction/v3.0/apps/'+self.app_id+'/slots/staging/predict/'
         response = requests.get(url, params=params,headers=headers)
         data = json.loads(response.content.decode('utf-8-sig'))
-        TownName = data['prediction']['entities']['TownName']
+        townName = data['prediction']['entities']['TownName']
         score = data['prediction']['intents']['getTowns']['score']
-        return data,score,TownName
+        return data,score,townName
 
 
 #curl "https://predictionintent.cognitiveservices.azure.com/luis/prediction/v3.0/apps/120aa983-ae16-4a7d-846c-f509628fb4b1/slots/staging/predict?verbose=true&show-all-intents=true&log=true&subscription-key=30534794ef1143c5917e76fe1d6d42bd&query=YOUR_QUERY_HERE"
